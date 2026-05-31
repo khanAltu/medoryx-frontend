8015c9392d5afddf776cfadfe4fba6c0d8566617
khanAltu <developerkhan009@gmail.com>
2026-05-31 13:34:37 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Login from './pages/Login'
-import './index.css' // Agar css file ka naam alag hai toh use badal sakte hain
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
